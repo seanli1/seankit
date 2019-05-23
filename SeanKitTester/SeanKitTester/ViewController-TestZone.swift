02599420aa172ed47e5d9328eb41d0541fc68aaf
Sean Li <seanlidev@gmail.com>
2019-05-23 14:55:57 -0400

    Added TestZone

---
+++ SeanKitTester/SeanKitTester/ViewController-TestZone.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController-TestZone.swift
+//  SeanKitTester
+//
+//  Created by Sean Li on 5/23/19.
+//  Copyright © 2019 86 Houses LLC. All rights reserved.
+//
+
+import SeanKit
+
+extension ViewController {
+    
+    @objc func runTestFunction() {
+        
+        // Run any test functions here.
+        
+    }
+    
+}
--- SeanKitTester/SeanKitTester/ViewController.swift
+++ SeanKitTester/SeanKitTester/ViewController.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 86 Houses LLC. All rights reserved.
 //
 
-import UIKit
 import SeanKit
 
 class ViewController: UIViewController, UICollectionViewDelegate {
@@ -21,17 +20,4 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         view.backgroundColor = UIColor.black
         runTestFunction()
     }
-    
-    @objc func runTestFunction() {
-        let filepath = Bundle.main.path(forResource: "localfile.txt", ofType: nil)
-        let text = try! String(contentsOfFile: filepath!)
-        
-        let array = Manipulate.textToArray(text, separator: "\n", ignoreBlank: true)
-        
-        for line in array {
-            print(line)
-        }
-    }
-    
-    
 }
