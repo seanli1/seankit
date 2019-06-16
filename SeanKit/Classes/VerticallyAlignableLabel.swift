7268aa7853b170a65c8b30fad3b64218e51d4145
Sean Li <seanlidev@gmail.com>
2019-06-16 17:57:06 -0400

    Added VerticallyAlignableLabel

---
+++ SeanKit/Classes/VerticallyAlignableLabel.swift
@@ -0,0 +1,29 @@
+//
+//  VerticallyAlignableLabel.swift
+//  SeanKit
+//
+//  Created by Sean Li on 6/16/19.
+//  Copyright © 2019 86 Houses LLC. All rights reserved.
+//
+
+import UIKit
+
+
+public class VerticallyAlignableLabel: UILabel {
+    
+    public override func drawText(in rect: CGRect) {
+        var newRect = rect
+        switch contentMode {
+        case .top:
+            newRect.size.height = sizeThatFits(rect.size).height
+        case .bottom:
+            let height = sizeThatFits(rect.size).height
+            newRect.origin.y += rect.size.height - height
+            newRect.size.height = height
+        default:
+            ()
+        }
+        super.drawText(in: newRect)
+    }
+}
+
