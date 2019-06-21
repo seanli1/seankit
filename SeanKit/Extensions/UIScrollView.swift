b946aa0323d26c0d08af64c905eaf28ad5ce0a95
Sean Li <seanlidev@gmail.com>
2019-06-21 01:55:30 -0400

    changed the updateHeightConstraint function to allow a predetermined width if any and a completion block to pass back the determined height

--- SeanKit/Extensions/UIScrollView.swift
+++ SeanKit/Extensions/UIScrollView.swift
@@ -19,13 +19,19 @@ public extension UIScrollView {
      Update a text view's height. Must have already created a height constraint that is a constant because this adjusts it.
      
      Insert this wherever needed (viewWillAppear, textViewDidChange, etc.)
+     
+     Provide `predeterminedWidth` to use that as a hard and reliable reference. If not, it will infer the object's current width. Note that sometimes, the inferred width will be 0.0 if called at the wrong time.
+     
+     Completion block gives you access to the height that gets determined.
      */
-    func updateHeightConstraint() {
+    func updateHeightConstraint(predeterminedWidth: CGFloat?, completion: ((CGFloat)-> Void)?) {
         self.isScrollEnabled = false
-        let estimatedSize = self.sizeThatFits(CGSize(width: self.frame.width, height: .infinity))
+        let estimatedSize = self.sizeThatFits(CGSize(width: predeterminedWidth ?? self.frame.width, height: .infinity))
+        let height = estimatedSize.height
         for constraint in self.constraints {
             if constraint.firstAttribute == .height {
-                constraint.constant = estimatedSize.height
+                constraint.constant = height
+                completion?(height)
             }
         }
     }
