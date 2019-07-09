fc674072d843eb3f54b5cc718cfeafffec3c6902
Sean Li <seanlidev@gmail.com>
2019-07-09 07:28:17 -0400

    Added convenience init for stack view

--- SeanKit/Extensions/UIStackView.swift
+++ SeanKit/Extensions/UIStackView.swift
@@ -11,6 +11,14 @@ import UIKit
 
 public extension UIStackView {
     
+    convenience init(_ axis: NSLayoutConstraint.Axis, _ distribution: UIStackView.Distribution, _ alignment: UIStackView.Alignment, spacing: CGFloat) {
+        self.init()
+        self.axis = axis
+        self.distribution = distribution
+        self.alignment = alignment
+        self.spacing = spacing
+    }
+    
     func addArrangedSubviews(_ views: [UIView]) {
         subviews.forEach { addArrangedSubview($0) }
     }
