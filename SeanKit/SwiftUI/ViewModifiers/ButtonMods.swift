760c05880956bb6a1789828b197669a4a92357a0
Sean Li <seanlidev@gmail.com>
2020-05-31 04:00:16 -0400

    Added view modifiers

---
+++ SeanKit/SwiftUI/ViewModifiers/ButtonMods.swift
@@ -0,0 +1,36 @@
+//
+//  ButtonMods.swift
+//  RequestLive
+//
+//  Created by Sean Li on 5/31/20.
+//  Copyright © 2020 Sean Li. All rights reserved.
+//
+
+import SwiftUI
+
+@available (iOS 13, *)
+public class SKViewMods {
+    private init() {}
+
+    /// Remember to add `.resizable()` before this modifier.
+    public struct NavBarBtnSize: ViewModifier {
+        
+        public func body(content: Content) -> some View {
+            content
+                .frame(width: 22, height: 22)
+        }
+        
+    }
+
+    /// Remember to add `.resizable()` before this modifier.
+    public struct TabBarBtnSize: ViewModifier {
+        
+        public func body(content: Content) -> some View {
+            content
+                .frame(width: 34, height: 34)
+        }
+    }
+}
+
+
+
