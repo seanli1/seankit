83f92077e8a65f774e9342a6f7ebf85e6b924a5b
Sean Li <seanlidev@gmail.com>
2020-06-06 23:53:18 -0400

    Added max height for scroll controller

--- SeanKit/SwiftUI/Views/SKScrollControlProxy.swift
+++ SeanKit/SwiftUI/Views/SKScrollControlProxy.swift
@@ -48,7 +48,7 @@ public class SKScrollControlProxy {
         case end
         case top
         case point(point: CGPoint)
-        case offset(point: CGPoint)
+        case offset(point: CGPoint, listHeight: CGFloat)
     }
 
     private var scrollView: UIScrollView?
@@ -70,16 +70,22 @@ public class SKScrollControlProxy {
                 case .point(let point):
                     rect.origin.y = point.y
                 scroller.scrollRectToVisible(rect, animated: animated)
-                case .offset(let point):
+                case .offset(let point, let height):
                     var newOffset = scroller.contentOffset
                     newOffset.y += point.y
+                    if newOffset.y < 0 {
+                        newOffset.y = 0
+                    }
+                    let maxHeight = scroller.contentSize.height - height - 1
+                    if newOffset.y > maxHeight {
+                        newOffset.y = maxHeight
+                    }
                     scroller.setContentOffset(newOffset, animated: animated)
                 default: {
-                    scroller.scrollRectToVisible(rect, animated: animated)
                     // default goes to top
+                    scroller.scrollRectToVisible(rect, animated: animated)
                 }()
             }
-//            scroller.scrollRectToVisible(rect, animated: false)
         }
     }
 }
