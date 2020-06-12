6912319244ecfcfa2ef2645879c2a63496d6d026
Sean Li <seanlidev@gmail.com>
2020-06-12 14:55:30 -0400

    Added some default sizes to CGFloat extension

---
+++ SeanKit/Extensions/CGFloat.swift
@@ -0,0 +1,20 @@
+//
+//  CGFloat.swift
+//  RequestLive
+//
+//  Created by Sean Li on 6/12/20.
+//  Copyright © 2020 Sean Li. All rights reserved.
+//
+
+import SwiftUI
+
+// Save default values for reference.
+
+// Values as of 6/12/2020
+
+extension CGFloat {
+    
+    static let navBarCardStyleHeight: CGFloat = 44
+    static let listCellHeight: CGFloat = 44
+    
+}
