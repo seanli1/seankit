01c6cbcb2b68ec0c169576c262b0f06bcfb413fc
Sean Li <seanlidev@gmail.com>
2020-06-18 21:08:09 -0400

    Added skactivityview

---
+++ SeanKit/SwiftUI/Views/SKActivityView.swift
@@ -0,0 +1,29 @@
+//
+//  SKActivityView.swift
+//  SeanKit
+//
+//  Created by Sean Li on 6/18/20.
+//  Copyright © 2020 86 Houses LLC. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13, *)
+public struct SKActivityView: UIViewRepresentable {
+    
+    public let scale: CGFloat
+    
+    public init(scale: CGFloat = 1) {
+        self.scale = scale
+    }
+    
+    public func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let view = UIActivityIndicatorView()
+        view.transform = CGAffineTransform(scaleX: scale, y: scale)
+        view.startAnimating()
+        return view
+    }
+    public func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        
+    }
+}
