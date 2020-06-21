fb2493fe088d5aa0e5f198f34061b91b27a49ba3
Sean Li <seanlidev@gmail.com>
2020-06-20 22:13:25 -0400

    Added SKShowMod

---
+++ SeanKit/SwiftUI/ViewModifiers/SKShowMod.swift
@@ -0,0 +1,30 @@
+//
+//  SKShowMod.swift
+//  SeanKit
+//
+//  Created by Sean Li on 6/20/20.
+//  Copyright © 2020 86 Houses LLC. All rights reserved.
+//
+
+import SwiftUI
+
+
+/// Pass boolean binding that determines if this is showing or not.
+@available(iOS 13, *)
+public struct SKShowMod: ViewModifier {
+    
+    @Binding var isShowing: Bool
+    
+    public init(_ isShowing: Binding<Bool>) {
+        self._isShowing = isShowing
+    }
+    
+    public func body(content: Content) -> some View {
+        Group {
+            if self.isShowing {
+                content
+            }
+        }
+    }
+    
+}
